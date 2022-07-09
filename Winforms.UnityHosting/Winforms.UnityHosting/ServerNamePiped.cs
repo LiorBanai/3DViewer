b963b57b4d41ae9fa25e788d80a1662696ef1fc3
Lior Banai <36262995+LiorBanai@users.noreply.github.com>
2022-07-09 21:10:57 +0300

    initial

---
+++ Winforms.UnityHosting/Winforms.UnityHosting/ServerNamePiped.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Pipes;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Winforms.UnityHosting
+{
+    public class ServerNamePiped
+    {
+        private NamedPipeServerStream namedPipeServerStream;
+        private Task Listen { get; set; }
+        public ServerNamePiped()
+        {
+            try
+            {
+                //Create Server Instance
+                namedPipeServerStream = new NamedPipeServerStream("Unity_Obj_Viewer", PipeDirection.InOut, 1);
+                //Wait for a Client to connect
+                namedPipeServerStream.WaitForConnection();
+                Listen = Task.Run(() =>
+                {
+                    StreamString serverStream = new StreamString(namedPipeServerStream);
+                });
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
+        public void Close()
+        {
+            namedPipeServerStream.Close();
+
+        }
+    }
+}
---
+++ Winforms.UnityHosting/Winforms.UnityHosting/UnityWindow.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Forms;
+
+namespace Winforms.UnityHosting
+{
+    public partial class UnityWindow : UserControl
+    {
+        private ServerNamePiped Pipe { get; }
+        public UnityWindow()
+        {
+            InitializeComponent();
+            Pipe = new ServerNamePiped();
+
+        }
+    }
+}
